b816fc8166886e16ead0963582777e2f5d9c9960
p-fedotov <paul.fedotov@gmail.com>
2016-11-29 01:59:46 +0300

    server schemas

---
+++ server/server/createdb.js
@@ -0,0 +1,61 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/crypto');
+
+var Schema = mongoose.Schema;
+
+var User = new Schema({
+	login: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	type: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+var Test = new Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	questions: {
+		type: Array,
+		required: true
+	} 
+});
+
+var Question = new Schema({
+	question: {
+		type: String,
+		required: true
+	},
+	answers: {
+		type: Array,
+		required: true
+	}
+});
+
+var Answer = new Schema({
+	answer: {
+		type: String,
+		required: true
+	},
+	isTrue: {
+		type: Boolean,
+		required: true
+	}
+});
