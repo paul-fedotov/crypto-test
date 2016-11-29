21dcd294bf68ed6d54a281cbba72a3dac532c382
p-fedotov <paul.fedotov@gmail.com>
2016-11-30 01:22:01 +0300

    dashboard templates server

--- server/index.js
+++ server/index.js
@@ -4,14 +4,18 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-//app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/bower_components'));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/login');
 });
+
+app.get('/index', function(request, response){
+	response.render('pages/index');
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
